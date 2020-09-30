5da8eafeaa9bfcc73ab7783102f9dd7a9f02a2c9
George Muraru <murarugeorgec@gmail.com>
2020-10-01 01:17:37 +0300

    feat(python trie): add trie implementation in Python
    Add Trie implementation, conda environment with packages for linting and test stubs
    
    fix #7

---
+++ Languages/Python/nice_implementations/src/trie/trie.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+
+
+class Trie:
+    def __init__(self):
+        def node():
+            return defaultdict(node)
+
+        self.root = node()
+
+    def insert(self, word: str):
+        node = self.root
+
+        for c in word:
+            node = node[c]
+
+        node[None]
+
+    def find(self, word: str) -> bool:
+        node = self.root
+
+        for c in word:
+            if c not in node:
+                return False
+
+            node = node[c]
+
+        return None in node
+
+    def delete(self, word: str) -> bool:
+        node = self.root
+
+        for c in word:
+            if c not in node:
+                return False
+
+        del node[None]
+        return True
---
+++ Languages/Python/nice_implementations/test/trie/test_trie.py
@@ -0,0 +1,14 @@
+def test_trie_add():
+    pass
+
+
+def test_trie_find():
+    pass
+
+
+def test_trie_delete():
+    pass
+
+
+def test_trie_mix():
+    pass
