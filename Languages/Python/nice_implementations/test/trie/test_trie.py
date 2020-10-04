f98e653900123f0d839c1aca3639ae392f14e51d
George Muraru <murarugeorgec@gmail.com>
2020-10-04 23:38:03 +0300

    test(tests): add missing tests and correct bug
    Add tests for find/delete (implicitly testing the insert function) elements in trie.
    Add 100% coverage for trie.
    
    fix #9

--- Languages/Python/nice_implementations/test/trie/test_trie.py
+++ Languages/Python/nice_implementations/test/trie/test_trie.py
@@ -1,14 +1,41 @@
-def test_trie_add():
-    pass
+from src.trie.trie import Trie
 
 
+    
 def test_trie_find():
-    pass
+    trie = Trie()
+
+    trie.insert("hello trie")
+    trie.insert("this is a string")
+    trie.insert("this is another string")
+
+
+    assert trie.find("hello trie")
+    assert trie.find("this is a string")
+    assert trie.find("this is another string")
+
+    assert not trie.find("hello")
+    assert not trie.find("this is an")
 
 
 def test_trie_delete():
-    pass
+    trie = Trie()
 
+    trie.insert("hello trie")
+    trie.insert("this is a string")
+    trie.insert("this is another string")
 
-def test_trie_mix():
-    pass
+    assert not trie.delete("helo")
+    assert not trie.delete("hello")
+    assert trie.delete("hello trie")
+
+    assert not trie.find("hello there")
+
+    assert not trie.delete("this is an")
+    assert trie.find("this is a string")
+
+    assert trie.delete("this is a string")
+    assert trie.find("this is another string")
+    
+    assert trie.delete("this is another string")
+    assert not trie.find("this is another string")
