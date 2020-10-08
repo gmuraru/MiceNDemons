ddc315d68cee798a1b7a1e60b60ed69016fa6f22
Varchasa Aggarwal <43700017+varchasa@users.noreply.github.com>
2020-10-08 22:35:27 +0530

    Update simpsons_rule.py

--- simpsons_rule.py
+++ simpsons_rule.py
@@ -1,21 +1,22 @@
 import numpy as np
-try:
-    ul = int(input("upper limit of the function : "))
-    ll = int(input("lower limit of the function : "))
-    n=4
-    h=(ul-ll)/n
+def func(s):
+    return np.sqrt(s)
+
+def simpsons_13rule(ul,ll,n):
     x=[]
     s=0
     fx=[]
+    h=(ul-ll)/n
     for i in range(0,n+1):
-        fx.append(np.sqrt(s))
+        fx.append(func(s))
         x.append(s)
         s=s+h
+
     s= 0
     for i in fx:
         if(fx.index(i) ==0 or fx.index(i) == len(fx)-1):
             s=s+i
-        
+            
     fx = fx[1:len(fx)-1]
     for i in fx:
         if(fx.index(i) % 2==0):
@@ -23,7 +24,13 @@ try:
         else:
             s=s+i*2
 
-    print("y = ", h*s/3)
+    return (h*s/3)
+try:
+    ul = int(input("upper limit of the function : "))
+    ll = int(input("lower limit of the function : "))
+    n=4
+    s=0
+    print("y : ",simpsons_13rule(ul,ll,n))
     
 except:
     print("Kindly enter valid input")
