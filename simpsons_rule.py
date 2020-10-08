12059d90dfc91c83c6d5ce2131de7f0c97cafe47
Varchasa Aggarwal <43700017+varchasa@users.noreply.github.com>
2020-10-09 01:16:12 +0530

    Update simpsons_rule.py

--- simpsons_rule.py
+++ simpsons_rule.py
@@ -1,17 +1,16 @@
 import numpy as np
-def func(s):
-    return np.sqrt(s)
 
-def simpsons_13rule(ul,ll,n):
+def simpsons_13rule(ul,ll,n,func):
     x=[]
     s=0
     fx=[]
     h=(ul-ll)/n
     for i in range(0,n+1):
-        fx.append(func(s))
+        func = np.cosh(s)
+        fx.append(func)
         x.append(s)
         s=s+h
-
+    
     s= 0
     for i in fx:
         if(fx.index(i) ==0 or fx.index(i) == len(fx)-1):
@@ -25,12 +24,24 @@ def simpsons_13rule(ul,ll,n):
             s=s+i*2
 
     return (h*s/3)
+
 try:
     ul = int(input("upper limit of the function : "))
     ll = int(input("lower limit of the function : "))
     n=4
     s=0
-    print("y : ",simpsons_13rule(ul,ll,n))
+    func = np.cosh(s)
+    print("y : ",simpsons_13rule(ul,ll,n,func))
     
 except:
     print("Kindly enter valid input")
+
+"""
+**TEST CASES**
+output for function simpsons_13rule(8,0,4,np.sin(s)) - 1.3301
+output for function simpsons_13rule(8,0,4,np.cos(s)) - 1.14887
+output for function simpsons_13rule(8,0,4,np.sinh(s)) - 1577.612
+output for function simpsons_13rule(8,0,4,np.cosh(s)) - 1578.6713
+"""
+   
+
